0ecd4e8d24e778e37ee7740f62ff4760b28e34de
Devaraj <67256594+Dev3537@users.noreply.github.com>
2025-07-03 11:32:04 +0530

    Optimizing controller

--- src/java_http_server/com/dev/application/Controller.java
+++ src/java_http_server/com/dev/application/Controller.java
@@ -3,7 +3,6 @@ package java_http_server.com.dev.application;
 public class Controller {
 
 	public static void main(String[] args) {
-		ServerSocketImage image = new ServerSocketImage();
-		image.listen();
+		new ServerSocketImage().listen();
 	}
 }
